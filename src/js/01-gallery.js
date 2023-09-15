5ffd3445b900baf5810a379fe6b3ffe4936dcdc6
Anna Vlasenko <elik@example.com>
2023-09-15 20:56:54 +0300

    tasck1-3

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(item =>
+    `<li class = "gallery__item">
+        <a class = "gallery__link" href="${item.original}"> 
+            <img
+            class ="gallery__image"
+            loading = 'lazy'
+            src="${item.preview}" 
+            alt="${item.description}"
+            />
+        </a>
+    </li>`);
+
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+
+const SimpleLightbox = new SimpleLightbox('.gallery a', {
+    captionsData:'alt',
+    captionDelay: 250,
+});
---
+++ src/js/02-video.js
@@ -0,0 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+   const iframe = document.querySelector('iframe');
+   const player = new Player(iframe);
+ 
+   const KEY_TIME = "videoplayer-current-time";
+
+   const onPlay = function(data) {
+       console.log('data.seconds');
+     localStorage.setItem(KEY_TIME, JSON.stringify(data.seconds));
+    };
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_TIME)) || 0);
